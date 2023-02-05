05b37384752cff410e618803637df17e4024282d
Dias12 <diasoryntayev@gmail.com>
2023-02-05 14:28:21 +0600

    change main

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -3,13 +3,25 @@ package main
 import (
 	"fmt"
 	"forum/internal/handler"
+	"forum/internal/repository"
 	"forum/internal/server"
+	"forum/internal/service"
 	"log"
 )
 
 const port = ":8888"
 
 func main() {
+	configDB := repository.NewConfDB()
+	db, err := repository.InitDB(configDB)
+	if err != nil {
+		log.Fatalf("failed to initialize db : %s", err.Error())
+	}
+	if err := repository.CreateTables(db); err != nil {
+		log.Fatal(err)
+	}
+	repo := repository.NewRepository(db)
+	services := service.NewService(*repo)
 	handler := handler.NewHandler(services)
 
 	server := new(server.Server)
