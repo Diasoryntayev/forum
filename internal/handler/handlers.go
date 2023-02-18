67410cc794ee02333aa0a7d11ccec8ea20846aea
Dias12 <diasoryntayev@gmail.com>
2023-02-18 11:39:05 +0600

    add tempExecute

--- internal/handler/handlers.go
+++ internal/handler/handlers.go
@@ -1,7 +1,9 @@
 package handler
 
 import (
+	"fmt"
 	"forum/internal/service"
+	"html/template"
 	"net/http"
 )
 
@@ -23,4 +25,19 @@ func (h *Handler) InitRoutes() http.Handler {
 	mux.HandleFunc("/post/create", h.createPost)
 	mux.HandleFunc("/my-posts", h.myPosts)
 	return mux
+}
+
+func (h *Handler) templExecute(w http.ResponseWriter, path string, data interface{}) error {
+	templ, err := template.ParseFiles(path)
+	if err != nil {
+		fmt.Println("ParseFiles()")
+		h.ErrorPage(w, http.StatusInternalServerError, http.StatusText(http.StatusInternalServerError))
+		return err
+	}
+	if err = templ.Execute(w, data); err != nil {
+		fmt.Println("templExecute()")
+		h.ErrorPage(w, http.StatusInternalServerError, http.StatusText(http.StatusInternalServerError))
+		return err
+	}
+	return nil
 }
