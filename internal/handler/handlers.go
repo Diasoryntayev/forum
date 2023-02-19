3528a6d7f38c9a03e3977ce2dcb6ce57eee697c6
Dias12 <diasoryntayev@gmail.com>
2023-02-19 19:41:51 +0600

    add all handlers

--- internal/handler/handlers.go
+++ internal/handler/handlers.go
@@ -19,11 +19,20 @@ func NewHandler(s *service.Service) *Handler {
 
 func (h *Handler) InitRoutes() http.Handler {
 	mux := http.NewServeMux()
+	mux.HandleFunc("/", h.userIdentity(h.home))
 	mux.HandleFunc("/auth/sign-up", h.signUp)
 	mux.HandleFunc("/auth/sign-in", h.signIn)
 	mux.HandleFunc("/log-out", h.logOut)
-	mux.HandleFunc("/post/create", h.createPost)
-	mux.HandleFunc("/my-posts", h.myPosts)
+	mux.HandleFunc("/post/create", h.userIdentity(h.createPost))
+	mux.HandleFunc("/my-posts", h.userIdentity(h.myPosts))
+	mux.HandleFunc("/my-favourites", h.userIdentity(h.myFavourites))
+	mux.HandleFunc("/post/", h.userIdentity(h.post))
+	mux.HandleFunc("/like-post", h.userIdentity(h.likePost))
+	mux.HandleFunc("/dislike-post", h.userIdentity(h.dislikePost))
+	mux.HandleFunc("/like-comment", h.userIdentity(h.likeComment))
+	mux.HandleFunc("/dislike-comment", h.userIdentity(h.dislikeComment))
+	mux.Handle("/ui/css/", http.StripPrefix("/ui/css/", http.FileServer(http.Dir("./ui/css/"))))
+	// handler := h.Logging(mux)
 	return mux
 }
 
